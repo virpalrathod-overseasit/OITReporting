a1f5a939dbbc2f8064d954440e370c6b76e0aff6
manojpatel-overseasit <manojpatel.overseasit@gmail.com>
2017-08-11 15:25:56 +0530

    Project Created

---
+++ OITReporting.Common/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OITReporting.Common
+{
+    public static class Enums
+    {
+        public enum Status
+        {
+            Active=1,
+            InActive=2
+        }
+    }
+}
---
+++ OITReporting.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OITReporting.Web.MvcApplication" Language="C#" %>
